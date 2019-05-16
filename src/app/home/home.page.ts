b3fe461ba2ae9aadf4c7e5ae8962ff15c2ccaad5
QuentinNev <neves.quentin@gmail.com>
2019-05-16 11:48:36 +0200

    Add lambda testing page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  constructor(private storage: Storage) {
+    //this.bottleProvider = new BottleProvider(this.storage)
+  }
 }

624e1209af677453d87cbc5ab24a9b81a23b29b8
Quentin Neves <neves.quentin@gmail.com>
2019-06-13 11:41:36 +0200

    Fix merge issues

---
+++ src/app/_providers/VineBatchProvider.ts
@@ -0,0 +1,34 @@
+import { Storage } from '@ionic/storage'
+import { VineBatch } from '../_models/VineBatch'
+import { Injectable } from '@angular/core'
+
+@Injectable()
+export class VineBatchProvider {
+  private storage: Storage
+  private storageKey: string
+  constructor(storage: Storage) {
+    this.storage = storage
+    this.storageKey = "batches"
+  }
+
+  public async addVineBatch(vineBatch: VineBatch) {
+    let batches = await this.getVineBatches()
+    batches = batches ? [...batches, vineBatch] : [vineBatch]
+    this.storage.set(this.storageKey, batches)
+  }
+
+  public async removeVineBatch(uuid) {
+    let batches = await this.getVineBatches()
+    if (batches) {
+      batches = batches.filter(function (batch) {
+        return batch.uuid !== uuid
+      })
+      this.storage.set(this.storageKey, batches)
+    }
+  }
+
+  // Returns a promise, use in an async function !
+  public getVineBatches() {
+    return this.storage.get(this.storageKey)
+  }
+}
